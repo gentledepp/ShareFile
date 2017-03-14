d3372ff1e896974c68ab10cc31d3c648000d9454
Alexander Marek <gentledepp@gmail.com>
2017-03-14 13:33:35 +0100

    Added UWP to nuspec and raised version to 0.0.3

---
+++ package.ps1
@@ -0,0 +1,10 @@
+$targetNugetExe = "$pwd\nuget.exe"
+
+if(-not (Test-Path $targetNugetExe)){
+	$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+	Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+}
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose 
+
+
+nuget pack plugin.sharefile.nuspec
